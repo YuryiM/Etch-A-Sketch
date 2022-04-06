72a971cf1a0ba16e3d41bd20bcd5f3f5bd80aae5
Yuryi Mironchyk <ymironchyk@gmail.com>
2022-04-05 23:02:57 -0700

    Added comments to js functions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ slider.defaultValue = 16;
 let sketchbox = document.getElementById('sketchbox')
 
 function generateGrid(){
-    // Default dimension 16x16
+    // Sets grid dimension
     dimension = slider.value;
     for(i = 0; i < dimension; i++){
         let row = document.createElement('div');
@@ -19,12 +19,14 @@ function generateGrid(){
 }
 
 function clearGrid(){
+    // While sketchbox still has children, remove them
     while(sketchbox.firstChild){
         sketchbox.removeChild(sketchbox.lastChild);
     }
 }
 
 slider.addEventListener('input', () => {
+    // Updates canvas size
     document.getElementById('canvas-size').innerHTML = slider.value + " x " + slider.value;
     clearGrid();
     generateGrid();
