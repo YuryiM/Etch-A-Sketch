636a9b9b88a61d0b67ce0dd93ddf216d6ca9d379
Yuryi Mironchyk <ymironchyk@gmail.com>
2022-04-06 12:20:37 -0700

    Added draw functionality

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 let slider = document.getElementById('size-slider');
 slider.defaultValue = 16;
 
-let sketchbox = document.getElementById('sketchbox')
+let sketchbox = document.getElementById('sketchbox');
+
+let pixels = document.getElementsByClassName('pixel');
+
+let brushOn = false;
 
 function generateGrid(){
-    // Sets grid dimension
+    // Sets grid dimension based on slider val
     dimension = slider.value;
     for(i = 0; i < dimension; i++){
+        // Creates row div
         let row = document.createElement('div');
         row.className = 'row';
         for(j = 0; j < dimension; j++){
+            // Creates the individual pixel
             let pixel = document.createElement('div');
             pixel.className = 'pixel';
             row.appendChild(pixel);
+            // When user mouses over pixel AND has their mouse down on canvas, set pixel to  black
+            pixel.addEventListener('mouseover', () => {
+                if(brushOn) pixel.style.backgroundColor = '#000000';
+            });
         }
         sketchbox.appendChild(row);
     }
 }
+
+
+// Turns on brush if mouse held pressed
+sketchbox.addEventListener('mousedown', () => {
+    brushOn = true;
+});
+
+sketchbox.addEventListener('mouseup', () => {
+    brushOn = false;
+});
+
+
 
 function clearGrid(){
     // While sketchbox still has children, remove them
@@ -25,8 +47,16 @@ function clearGrid(){
     }
 }
 
+Array.prototype.forEach.call(pixels, (pixel)=> {
+
+    console.log('HEY111');
+    
+}, false);
+
+
+
 slider.addEventListener('input', () => {
-    // Updates canvas size
+    // Updates canvas size when slider changed
     document.getElementById('canvas-size').innerHTML = slider.value + " x " + slider.value;
     clearGrid();
     generateGrid();
