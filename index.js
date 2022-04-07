ee599e326b08e4c9eea22c6e5308f4eced76db70
Yuryi Mironchyk <ymironchyk@gmail.com>
2022-04-06 18:45:55 -0700

    Added rainbow mode functionality

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+// Creates elements to be used later
+
 let slider = document.getElementById('size-slider');
 
 let sketchbox = document.getElementById('sketchbox');
 
 let pixels = document.getElementsByClassName('pixel');
 
-let brushOn = false;
-
-let color = document.getElementById('color').value;
-
 let colorPicker = document.getElementById('color-pick');
+
+let eraser = document.getElementById('eraser-toggle');
+
+let rainbowButton = document.getElementById('rainbow-mode-toggle');
+
+let brushOn = false;
 
 let rainbowMode = false;
 
+let color = document.getElementById('color').value;
+
 let rainbowRed, rainbowGreen, rainbowBlue = 0;
 
-let eraser = document.getElementById('eraser-toggle');
+const rainbowColors = ['#ff0000', '#ffa500', '#ffff00', '#008000', '#0000ff', '#4b0082', '#ee82ee'];
+
+let r = 0;
+
+let currentRainbowColor = rainbowColors[r];
 
 document.getElementById('color').addEventListener('input', () => {
     color = document.getElementById('color').value;
     colorPicker.style.backgroundColor = color;
+    rainbowMode = false;
 });
 
 // Clears canvas
@@ -26,6 +37,18 @@ document.getElementById('clear-button').addEventListener('click', () => {
     Array.prototype.forEach.call(pixels, (pixel) => {
         pixel.style.backgroundColor = 'white';
     });
+});
+
+// Turns on rainbow mode
+rainbowButton.addEventListener('mousedown', () => {
+    if(rainbowButton.value == 'OFF'){
+        rainbowButton.value = 'ON';
+        rainbowMode = true;
+    }
+    else if (rainbowButton.value == 'ON'){
+        rainbowButton.value = 'OFF';
+        rainbowMode = false;
+    }
 });
 
 // Turns on eraser 
@@ -55,14 +78,21 @@ function generateGrid(){
             // When user mouses over pixel AND has their mouse down on canvas, set pixel to  black
             pixel.addEventListener('mouseover', () => {
                 if(brushOn){
+                    if(rainbowMode){
+                        pixel.style.backgroundColor = currentRainbowColor;
+                        if(r == rainbowColors.length-1) r = 0;
+                        else r++;
+                        currentRainbowColor = rainbowColors[r];
+                        }
+                    else {
                     pixel.style.backgroundColor = color;
+                    }
                 }
             });
         }
         sketchbox.appendChild(row);
     }
 }
-
 
 // Turns on brush if mouse held pressed
 sketchbox.addEventListener('mousedown', () => {
