e5f64da2943f189986349cad58a06b5071ab95f6
Yuryi Mironchyk <ymironchyk@gmail.com>
2022-04-06 21:24:20 -0700

    Styled control panel, added comments
    Final commit on this project unless I catch something.

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-// Creates elements to be used later
-
+// Buttons and sliders
+let colorPicker = document.getElementById('color-pick');
+let rainbowButton = document.getElementById('rainbow-mode-toggle');
+let eraserButton = document.getElementById('eraser-toggle');
+let clearButton = document.getElementById('clear-button');
 let slider = document.getElementById('size-slider');
 
+// Variables related to canvas
 let sketchbox = document.getElementById('sketchbox');
-
 let pixels = document.getElementsByClassName('pixel');
 
-let colorPicker = document.getElementById('color-pick');
-
-let eraser = document.getElementById('eraser-toggle');
-
-let rainbowButton = document.getElementById('rainbow-mode-toggle');
-
+// State of tools
 let brushOn = false;
-
 let eraserOn = false;
-
 let rainbowMode = false;
 
+// Current color, excluding rainbow mode
 let color = document.getElementById('color').value;
-
+// List of rainbow mode colors to cycle through
 const rainbowColors = ['#ff0000', '#ffa500', '#ffff00', '#008000', '#0000ff', '#4b0082', '#ee82ee'];
-
+// Current rainbow mode color
 let r = 0;
-
+// Current rainbow color
 let currentRainbowColor = rainbowColors[r];
 
+// Color input
 document.getElementById('color').addEventListener('input', () => {
     color = document.getElementById('color').value;
     colorPicker.style.backgroundColor = color;
@@ -34,34 +32,40 @@ document.getElementById('color').addEventListener('input', () => {
 });
 
 // Clears canvas
-document.getElementById('clear-button').addEventListener('click', () => {
+clearButton.addEventListener('mousedown', () => {
+    clearButton.style.backgroundColor = '#0064c2';
     Array.prototype.forEach.call(pixels, (pixel) => {
         pixel.style.backgroundColor = 'white';
     });
 });
 
+clearButton.addEventListener('mouseup', () => clearButton.style.backgroundColor = '#0082fc');
+
 // Turns on rainbow mode
 rainbowButton.addEventListener('mousedown', () => {
     if(rainbowButton.value == 'OFF'){
+        rainbowButton.style.backgroundColor = '#0064c2';
         rainbowButton.value = 'ON';
         rainbowMode = true;
-        eraserOn = false;
     }
     else if (rainbowButton.value == 'ON'){
+        rainbowButton.style.backgroundColor = '#0082fc';
         rainbowButton.value = 'OFF';
         rainbowMode = false;
     }
 });
 
 // Turns on eraser 
-eraser.addEventListener('mousedown', () => {
-    if(eraser.value == 'OFF'){
+eraserButton.addEventListener('mousedown', () => {
+    if(eraserButton.value == 'OFF'){
+        eraserButton.style.backgroundColor = '#0064c2';
         eraserOn = true;
-        eraser.value = 'ON';
+        eraserButton.value = 'ON';
     }
-    else if (eraser.value == 'ON'){
+    else if (eraserButton.value == 'ON'){
+        eraserButton.style.backgroundColor = '#0082fc';
         eraserOn = false;
-        eraser.value = 'OFF';
+        eraserButton.value = 'OFF';
         color = document.getElementById('color').value;;
     }
 });
@@ -103,13 +107,9 @@ function generateGrid(){
 }
 
 // Turns on brush if mouse held pressed
-sketchbox.addEventListener('mousedown', () => {
-    brushOn = true;
-});
-
-sketchbox.addEventListener('mouseup', () => {
-    brushOn = false;
-});
+sketchbox.addEventListener('mousedown', () => brushOn = true );
+// Turns off brush if mouse released
+sketchbox.addEventListener('mouseup', () => brushOn = false );
 
 function clearGrid(){
     // While sketchbox still has children, remove them
@@ -120,7 +120,7 @@ function clearGrid(){
 
 slider.addEventListener('input', () => {
     // Updates canvas size when slider changed
-    document.getElementById('canvas-size').innerHTML = slider.value + " x " + slider.value;
+    document.getElementById('canvas-size').innerHTML = slider.value + ' x ' + slider.value;
     clearGrid();
     generateGrid();
 }, false);
